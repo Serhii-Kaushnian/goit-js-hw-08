a42533ac66e59d9616b1a9fe4eddbcc7d4af949a
Serhii Kaushnian <skaushnjan@gmail.com>
2022-07-26 19:51:38 +0200

    amend

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.css';
+
+const ulEl = document.querySelector('.gallery');
+const previewLinks = galleryItems.reduce(
+  (acc, { preview, original, description }) => {
+    return (
+      acc +
+      `
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-lightbox="lbox"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+`
+    );
+  },
+  ''
+);
+ulEl.insertAdjacentHTML('beforeend', previewLinks);
+
+// var lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+//   captionPosition: 'bottom',
+// });
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  showCounter: false,
+  nextOnImageClick: true,
+  scrollZoom: false,
+});
